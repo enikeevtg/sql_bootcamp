775b99726ea1f190abc4ad0bb887de232b719bf3
Tagir Enikeev <enikeev.tg@gmail.com>
2023-11-04 15:18:39 +0300

    day08

---
+++ day08/day08_ex05.sql
@@ -0,0 +1,12 @@
+/*Session #1*/
+BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SELECT SUM(rating) FROM pizzeria;
+SELECT SUM(rating) FROM pizzeria;
+COMMIT;
+SELECT SUM(rating) FROM pizzeria;
+
+/*Session #2*/
+BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED;
+UPDATE pizzeria SET rating = 1.0 WHERE name = 'Pizza Hut';
+COMMIT;
+SELECT SUM(rating) FROM pizzeria;
---
+++ day08/day08_ex07.sql
@@ -0,0 +1,11 @@
+/*Session #1*/
+BEGIN;
+UPDATE pizzeria SET rating = 1.0 WHERE id = 1;
+UPDATE pizzeria SET rating = 3.0 WHERE id = 2;
+COMMIT;
+
+/*Session #2*/
+BEGIN;
+UPDATE pizzeria SET rating = 2.0 WHERE id = 2;
+UPDATE pizzeria SET rating = 4.0 WHERE id = 1;
+COMMIT;
